124ba18550210a81612d3a21a51e9cc33317b876
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-06-02 17:36:18 +0200

    Set hostname via terraform
    Fixes #6

--- terraform/machine/ignition.tf
+++ terraform/machine/ignition.tf
@@ -2,10 +2,23 @@ locals {
   ignition_encoded = "data:text/plain;charset=utf-8;base64,${base64encode(var.ignition)}"
 }
 
+data "ignition_file" "hostname" {
+  count = "${var.instance_count}"
+
+  filesystem = "root"
+  path = "/etc/hostname"
+  content {
+    content = "${var.name}-${count.index}"
+  }
+  mode = "0644"
+}
+
 data "ignition_config" "ign" {
   count = "${var.instance_count}"
 
   append {
     source = "${var.ignition_url != "" ? var.ignition_url : local.ignition_encoded}"
   }
+
+  files = ["${data.ignition_file.hostname.*.id[count.index]}"]
 }
